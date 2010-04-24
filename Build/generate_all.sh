f4885af09df881783082cc2c1a5a352c50d37d70
Cameron Hart <cam@bitshifter.net.nz>
2010-04-24 17:56:15 +1200

    Adpopted the premake idea of specifying where to generate build config with --to. Added Mac resources. Some premake bugs mean Mac *.lproj directories must be in the same diretory as premake4.lua.

--- Build/generate_all.sh
+++ Build/generate_all.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-premake4 clean
-premake4 --os=linux gmake
-premake4 --os=macosx xcode3
-premake4 --os=windows vs2008
+rm -rf Linux
+rm -rf Xcode
+rm -rf VC9
+premake4 --to=Linux --os=linux gmake
+premake4 --to=Xcode --os=macosx xcode3
+premake4 --to=VC9 --os=windows vs2008
--- Build/premake4.lua
+++ Build/premake4.lua
@@ -1,102 +1,105 @@
+--
+-- Use the --to=path option to control where the project files get generated. I use
+-- this to create project files for each supported toolset, each in their own folder,
+-- in preparation for deployment.
+--
+newoption {
+	trigger = "to",
+	value   = "path",
+	description = "Set the output location for the generated files"
+}
+
+if _OPTIONS["to"] ~= nil then
+	todir = _OPTIONS["to"]
+else
+	todir = "."
+end
+
 solution "recastnavigation"
-  configurations { "Debug", "Release" }
+	configurations { "Debug", "Release" }
+	location ( todir )
 
-  -- manual cleanup of generated builds
-  if _ACTION == "clean" then
-    os.rmdir("linux")
-    os.rmdir("vs2008")
-    os.rmdir("xcode3")
-  end
-
-  -- default linux makefiles to the linux dir, otherwise use the action
-  if _OS == "linux" and _ACTION == "gmake" then
-    default_location = "linux"
-  elseif _ACTION ~= nil then
-    default_location = _ACTION
-  end
-  
-  if default_location ~= nil then
-    location ( default_location )
-  end
-    
-  -- extra warnings, no exceptions or rtti
-  flags { "ExtraWarnings", "NoExceptions", "NoRTTI" }
-  
-  -- debug configs
-  configuration "Debug*"
-    targetsuffix "D"
-    defines { "DEBUG" }
-    flags { "Symbols" }
+	-- extra warnings, no exceptions or rtti
+	flags { "ExtraWarnings", "NoExceptions", "NoRTTI" }
+	
+	-- debug configs
+	configuration "Debug*"
+		targetsuffix "D"
+		defines { "DEBUG" }
+		flags { "Symbols" }
  
- -- release configs
-  configuration "Release*"
-    defines { "NDEBUG" }
-    flags { "Optimize" }
-  
-  -- windows specific
-  configuration "windows"
-    defines { "WIN32", "_WINDOWS", "_CRT_SECURE_NO_WARNINGS" }
-    
+ 	-- release configs
+	configuration "Release*"
+		defines { "NDEBUG" }
+		flags { "Optimize" }
+	
+	-- windows specific
+	configuration "windows"
+		defines { "WIN32", "_WINDOWS", "_CRT_SECURE_NO_WARNINGS" }
+	
+	
 project "DebugUtils"
-  language "C++"
-  kind     "StaticLib"
-  if default_location ~= nil then
-    location ( default_location )
-    targetdir ( default_location .. "/lib" )
-  end
-  includedirs { "../DebugUtils/Include", "../Detour/Include", "../Recast/Include" }
-  files { "../DebugUtils/Include/*.h", "../DebugUtils/Source/*.cpp" }
+	language "C++"
+	kind "StaticLib"
+	location ( todir )
+	includedirs { "../DebugUtils/Include", "../Detour/Include", "../Recast/Include" }
+	files { "../DebugUtils/Include/*.h", "../DebugUtils/Source/*.cpp" }
+	targetdir ( todir .. "/lib" )
 
 project "Detour"
-  language "C++"
-  kind     "StaticLib"
-  if default_location ~= nil then
-    location ( default_location )
-    targetdir ( default_location .. "/lib" )
-  end
-  includedirs { "../Detour/Include" }
-  files { "../Detour/Include/*.h", "../Detour/Source/*.cpp" }
+	language "C++"
+	kind "StaticLib"
+	location ( todir )
+	includedirs { "../Detour/Include" }
+	files { "../Detour/Include/*.h", "../Detour/Source/*.cpp" }
+	targetdir ( todir .. "/lib" )
 
 project "Recast"
-  language "C++"
-  kind "StaticLib"
-  if default_location ~= nil then
-    location ( default_location )
-    targetdir ( default_location .. "/lib" )
-  end
-  includedirs { "../Recast/Include" }
-  files { "../Recast/Include/*.h", "../Recast/Source/*.cpp" }
-  
+	language "C++"
+	kind "StaticLib"
+	location ( todir )
+	includedirs { "../Recast/Include" }
+	files { "../Recast/Include/*.h", "../Recast/Source/*.cpp" }
+	targetdir ( todir .. "/lib" )
+	
 project "RecastDemo"
-  language "C++"
-  kind     "WindowedApp"
-  if default_location ~= nil then
-    location ( default_location )
-  end
-  includedirs { "../RecastDemo/Include", "../RecastDemo/Contrib", "../DebugUtils/Include", "../Detour/Include", "../Recast/Include" }
-  files  { "../RecastDemo/Include/*.h", "../RecastDemo/Source/*.cpp" }
-  
-  -- project dependencies
-  links { "DebugUtils", "Detour", "Recast" }
+	language "C++"
+	kind "WindowedApp"
+	location ( todir )
+	includedirs { "../RecastDemo/Include", "../RecastDemo/Contrib", "../DebugUtils/Include", "../Detour/Include", "../Recast/Include" }
+	files	{ "../RecastDemo/Include/*.h", "../RecastDemo/Source/*.cpp" }
+	
+	-- project dependencies
+	links { "DebugUtils", "Detour", "Recast" }
 
-  -- distribute executable in RecastDemo/Bin directory
-  targetdir "../RecastDemo/Bin"
-  
-  -- linux library cflags and libs
-  configuration { "linux", "gmake" }
-    buildoptions { "`pkg-config --cflags sdl`", "`pkg-config --cflags gl`", "`pkg-config --cflags glu`" }
-    linkoptions { "`pkg-config --libs sdl`", "`pkg-config --libs gl`", "`pkg-config --libs glu`" }
-  
-  -- windows library cflags and libs
-  configuration { "windows" }
-    includedirs { "../RecastDemo/Contrib/SDL/include" }
-    libdirs { "../RecastDemo/Contrib/SDL/lib" }
-    links { "opengl32", "glu32", "sdlmain", "sdl" }
+	-- distribute executable in RecastDemo/Bin directory
+	targetdir "../RecastDemo/Bin"
+	
+	-- linux library cflags and libs
+	configuration { "linux", "gmake" }
+		buildoptions { "`pkg-config --cflags sdl`", "`pkg-config --cflags gl`", "`pkg-config --cflags glu`" }
+		linkoptions { "`pkg-config --libs sdl`", "`pkg-config --libs gl`", "`pkg-config --libs glu`" }
+	
+	-- windows library cflags and libs
+	configuration { "windows" }
+		includedirs { "../RecastDemo/Contrib/SDL/include" }
+		libdirs { "../RecastDemo/Contrib/SDL/lib" }
+		links { "opengl32", "glu32", "sdlmain", "sdl" }
  
-   -- mac includes and libs
-   configuration { "macosx" }
-     includedirs { "/Library/Frameworks/SDL.framework/Headers" }
-     buildoptions { "-Wreorder -Wsign-compare" }
-     linkoptions { "-framework OpenGL", "-framework SDL", "-framework Cocoa" }
-     files { "../RecastDemo/Include/SDLMain.h", "../RecastDemo/Source/SDLMain.m" }
-
+	 -- mac includes and libs
+	 configuration { "macosx" }
+		 includedirs { "/Library/Frameworks/SDL.framework/Headers" }
+		 buildoptions { "-Wreorder -Wsign-compare" }
+		 links { 
+		 	"OpenGL.framework", 
+		 	"/Library/Frameworks/SDL.framework", 
+		 	"Cocoa.framework",
+		 }
+		 files {
+		 	"../RecastDemo/Include/SDLMain.h", 
+		 	"../RecastDemo/Source/SDLMain.m",
+		 	"Info.plist",
+		 	"Icon.icns",
+		 	"English.lproj/InfoPlist.strings",
+		 	"English.lproj/MainMenu.xib",
+		 }
